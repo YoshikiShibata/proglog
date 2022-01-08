023016df44c48d0f247a3bbda221048dc1cd6991
Yoshiki Shibata <yoshiki.shibata@gmail.com>
2022-01-08 11:12:42 +0900

    updated

--- WriteALogPackage/internal/log/store.go
+++ WriteALogPackage/internal/log/store.go
@@ -34,3 +34,36 @@ func newStore(f *os.File) (*store, error) {
 		buf:  bufio.NewWriter(f),
 	}, nil
 }
+
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+	w += lenWidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
+
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
