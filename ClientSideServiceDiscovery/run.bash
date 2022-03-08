6cf5d0568566a58981f56c116c55eee48d4799dc
Yoshiki Shibata <yoshiki.shibata@mercari.com>
2022-03-09 05:50:36 +0900

    removed

--- ClientSideServiceDiscovery/run.bash
+++
@@ -1,4 +0,0 @@
-go clean -testcache
-go test github.com/travisjeffery/proglog/internal/agent
-go test github.com/travisjeffery/proglog/internal/discovery
-go test github.com/travisjeffery/proglog/internal/log
